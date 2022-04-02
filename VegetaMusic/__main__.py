ebae541074f51c56c2de0f82a9f8ba6db92a678e
Cute Inspire <Randywahidin321@gmail.com>
2022-04-02 08:50:20 +0700

    Update __main__.py

--- VegetaMusic/__main__.py
+++ VegetaMusic/__main__.py
@@ -1,21 +1,75 @@
-import requests
-from pyrogram import Client as Bot
+# Credits @TeamYukki 
+# by @rencprx 
 
-from VegetaMuic.config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from VegetaMusic.services.callsmusic import run
+import asyncio
+import importlib
+import sys
 
-response = requests.get(BG_IMAGE)
-file = open("./etc/foreground.png", "wb")
-file.write(response.content)
-file.close()
+from pyrogram import idle
+from pytgcalls.exceptions import NoActiveGroupCall
 
-bot = Bot(
-    ":memory:",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="VegetaMusic.modules"),
-)
+import config
+from config import BANNED_USERS
+from VegetaMusic import LOGGER, app, userbot
+from VegetaMusic.core.call import Vegeta
+from VegetaMusic.plugins import ALL_MODULES
+from VegetaMusic.utils.database import get_banned_users, get_gbanned
 
-bot.start()
-run()
+loop = asyncio.get_event_loop()
+
+
+async def init():
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER("VegetaMusic").error(
+            "No Assistant Clients Vars Defined!.. Exiting Process."
+        )
+        return
+    if (
+        not config.SPOTIFY_CLIENT_ID
+        and not config.SPOTIFY_CLIENT_SECRET
+    ):
+        LOGGER("VegetaMusic").warning(
+            "No Spotify Vars defined. Your bot won't be able to play spotify queries."
+        )
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
+    except:
+        pass
+    await app.start()
+    for all_module in ALL_MODULES:
+        importlib.import_module("VegetaMusic.plugins" + all_module)
+    LOGGER("Vegetamusic.plugins").info(
+        "Successfully Imported Modules "
+    )
+    await userbot.start()
+    await Vegeta.start()
+    try:
+        await Vegeta.stream_call(
+            "http://docs.evostream.com/sample_content/assets/sintel1m720p.mp4"
+        )
+    except NoActiveGroupCall:
+        LOGGER("VegetaMusic").error(
+            "[ERROR] - \n\nPlease turn on your Logger Group's Voice Call. Make sure you never close/end voice call in your log group"
+        )
+        sys.exit()
+    except:
+        pass
+    await Vegeta.decorators()
+    LOGGER("VegetaMusic").info("Vegeta Music Bot Started Successfully")
+    await idle()
+
+
+if __name__ == "__main__":
+    loop.run_until_complete(init())
+    LOGGER("VegetaMusic").info("Stopping Vegeta Music Bot! GoodBye")
